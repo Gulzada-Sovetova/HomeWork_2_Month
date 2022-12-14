fa2ad063ef60d074e016777ba29fb2c1b6202b1a
Gulzada-Sovetova <120395007+Gulzada-Sovetova@users.noreply.github.com>
2022-12-14 17:30:51 +0600

    Add files via upload

---
+++ DZ1_Gulzada_21-2/apps.js
@@ -0,0 +1,71 @@
+/*Дз-1
+Задача - 1
+написать программу: нужно запрашивать у пользователя его имя и фамилию 
+(отдельным prompt) и выводить приветствие в виде: 
+Здравствуйте, Имя фамилия!
+*/
+//Код решения
+
+
+// var customerName = prompt("Укажите своё имя");
+// var customerSurname = prompt("Укажите свою фамилию");
+// alert("Здравствуйте " + customerSurname + " " + customerName);
+// console.log("Здравствуйте " + customerSurname + " " + customerName);
+
+
+/*Задача - 2
+Написать программу которая спрашивает у пользователя 2 числа (через prompt)
+и выводит в консоль какое большое. Если числа равны, то вывести что они равны.
+*/
+//Код решения
+
+// var numberOne = Number(prompt("Введите первое число")); 
+// var numberTwo = Number(prompt("ВВведите второе число"));
+
+// if(numberOne > numberTwo ){
+//     console.log("Первое число больше второго")
+// }
+//  else if(numberOne < numberTwo ){
+//     console.log("Первое число меньше второго")
+// }
+// else if(numberOne === numberTwo ){
+//     console.log("Числа равны")
+// } else {    
+//     console.log("ERROR");
+
+// }
+
+/*Задача - 3
+написать программу "светофор": программа спрашивает у пользователя цвет в 
+текстовом виде, и печатает консоль действие согласно ПДД. 
+Например: красный - стой!, желтый - жди и.т.д
+*/
+//Код решения
+
+// var trafficLight = prompt ("Введите цвет светофора");
+// if (trafficLight == "" || trafficLight == undefined) {
+//     console.log("Не введено");
+// } else if (trafficLight ==="красный"){
+//     console.log("Стой!");
+// }else if (trafficLight ==="желтый"){
+//     console.log("Жди!");
+// }else if (trafficLight ==="зеленый"){
+//     console.log("Переходи!");
+// }else {
+//     console.log("Другое значение");
+// }  
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// var trafficLight =Number( prompt ("Введите цвет светофора : 1.Красный 2.Жёлтый 3.Зелёный"));
+//  if (trafficLight ===1){
+//     console.log("Стой!");
+// }else if (trafficLight ===2){
+//     console.log("Жди!");
+// }else if (trafficLight ===3){
+//     console.log("Переходи!");
+// }else {
+//     console.log("error");
+// }  
+
+
